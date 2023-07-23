359107c07d69996d3c606c8cf7908f440036b912
Sradelat <shawnradelat@gmail.com>
2023-07-23 19:02:13 -0400

    Added new columns/Generated data/SQL Queries

--- DataFaker.py
+++ DataFaker.py
@@ -5,15 +5,22 @@ import time
 from pprint import pprint
 import json
 
+
 headers = [
     {
         "title": "",
+        "company": "",
         "genre": "",
         "year": "",
-        "score": "",
-        "number_of_reviews": "",
+        "rating": "",
+        "length": "",
+        "critic_score": "",
+        "number_of_critics": "",
+        "user_score": "",
+        "number_of_users": "",
         "latitude": "",
-        "longitude": ""
+        "longitude": "",
+        "state_filmed": ""
     }
 ]
 
@@ -25,7 +32,7 @@ with open("Random.csv", "w", newline="") as f:
 vowels = "aeiou"
 with open("WordsAsLetters.json") as j_file:
     words = json.loads(j_file.read())
-    for i in range(50):
+    for i in range(2001):  # change range for amount of data entries
         title = f"{random.choice(words['words']['object_nouns'])} " \
                 f"{random.choice(words['words']['prepositions'])} " \
                 f"{random.choice(words['words']['concept_nouns'])}"
@@ -33,28 +40,45 @@ with open("WordsAsLetters.json") as j_file:
         if article_choice == 1:
             title = f"the {title}"
         else:
-            if title[0] in vowels:
+            if title[0] in vowels:  # if title starts with a vowel
                 title = f"an {title}"
             else:
                 title = f"a {title}"
-        score = random.choice(range(100))
+        user_score = random.choice(range(101))
+        no_of_users = random.choice(range(3001))
+        critic_score = random.choice(range(101))
+        no_of_critics = random.choice(range(101))
         genre = random.choice(words['words']['genres'])
-        no_of_reviews = random.choice(range(3000))
-        year = random.choice(range(1974, 2023))
+        year = random.choice(range(1974, 2024))  # maybe edit this to be a full date
+        rating = random.choice(words['words']['movie_ratings'])
+        length = random.choice(range(60, 121))
+        company = random.choice(words['words']['companies'])
 
-        resp = requests.get("https://api.3geonames.org/?randomland=US&json=1")
+        resp = requests.get("https://api.3geonames.org/?randomland=US&json=1")  # Free API gives random lat/long in US
+        print(resp)
+        print(resp.content)
         json = resp.json()
+
         lat = json["major"]["inlatt"]
         long = json["major"]["inlongt"]
+        state = json["major"]["prov"]
+        # pprint(json)
+
         categories = [
             {
                 "title": title.title(),
+                "company": company,
                 "genre": genre,
                 "year": year,
-                "score": score,
-                "number_of_reviews": no_of_reviews,
+                "rating": rating,
+                "length": length,
+                "critic_score": critic_score,
+                "number_of_critics": no_of_critics,
+                "user_score": user_score,
+                "number_of_users": no_of_users,
                 "latitude": lat,
-                "longitude": long
+                "longitude": long,
+                "state_filmed": state
             }
         ]
         with open("Random.csv", "a", newline="") as f:
@@ -63,4 +87,4 @@ with open("WordsAsLetters.json") as j_file:
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
         print(current_time)
-        # time.sleep(1)
+        time.sleep(5)
